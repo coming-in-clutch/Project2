45e48eeefc822f377a050401cbe20cd3e7c06d57
Austin Bird <austin.bird@byu.edu>
2017-12-14 08:53:23 -0700

    fixed some bugs with the image display and the routing to the selectedMission page

--- Project2/Controllers/AccountController.cs
+++ Project2/Controllers/AccountController.cs
@@ -166,7 +166,7 @@ namespace Project2.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Missions", "Home");
                 }
                 AddErrors(result);
             }
